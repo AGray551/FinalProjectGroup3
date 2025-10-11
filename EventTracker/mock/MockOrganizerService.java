f60ac77c908a4046ea18b7811085874d3a1dc8fb
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-10-11 12:22:21 -0400

    Create MockOrganizerService.java

---
+++ EventTracker/mock/MockOrganizerService.java
@@ -0,0 +1,22 @@
+package mock;
+ 
+import service.IOrganizerService;
+import model.Event;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class MockOrganizerService implements IOrganizerService {
+    private List<Event> createdEvents = new ArrayList<>();
+ 
+    @Override
+    public void createEvent(Event event) {
+        createdEvents.add(event);
+    }
+ 
+    @Override
+    public void deleteEvent(String eventId) {
+        createdEvents.removeIf(e -> e.getId().equals(eventId));
+    }
+}
+ 
