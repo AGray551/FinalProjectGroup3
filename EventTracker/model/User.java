f86e8df6638f4cab487fae6ed53223a186da4c36
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-10-11 12:19:06 -0400

    Create User.java

---
+++ EventTracker/model/User.java
@@ -0,0 +1,27 @@
+package model;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private List<String> rsvpedEvents = new ArrayList<>();
+ 
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+ 
+    public void rsvp(String eventId) {
+        rsvpedEvents.add(eventId);
+    }
+ 
+    public List<String> getRsvpedEvents() {
+        return rsvpedEvents;
+    }
+ 
+    // Getters and setters…
+}
