d28c0f32901d0138392db8ecbf2836e27a543984
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-10-11 12:21:47 -0400

    Create MockUserService.java

---
+++ EventTracker/mock/MockUserService.java
@@ -0,0 +1,21 @@
+package mock;
+ 
+import service.IUserService;
+import model.User;
+ 
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class MockUserService implements IUserService {
+    private Map<String, User> users = new HashMap<>();
+ 
+    @Override
+    public User getUserById(String userId) {
+        return users.get(userId);
+    }
+ 
+    @Override
+    public void createUser(User user) {
+        users.put(user.getId(), user);
+    }
+}
