c771195f62da24a7c345ee33c7cadda56a1f2f2d
Silas Curry <109922169+sil6s@users.noreply.github.com>
2025-10-30 15:59:29 -0400

    feat(application): add main application class for Spring Boot setup

---
+++ EventTracker/src/main/java/edu/uc/eventtracker/EventTrackerApplication.java
@@ -0,0 +1,11 @@
+package edu.uc.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventTrackerApplication.class, args);
+    }
+}
