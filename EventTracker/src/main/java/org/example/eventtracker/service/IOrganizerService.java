0594fc65fe37f6dc692b6223007be1df379597bb
ocahavens <ocahavens@gmail.com>
2025-10-30 19:02:38 -0400

    Add service interfaces for Event, Organizer, and User management

--- EventTracker/src/main/java/org/example/eventtracker/service/IOrganizerService.java
+++ EventTracker/src/main/java/org/example/eventtracker/service/IOrganizerService.java
@@ -1,7 +1,7 @@
-package service;
- 
-import model.Event;
- 
+package org.example.eventtracker.service;
+
+import org.example.eventtracker.model.Event;
+
 public interface IOrganizerService {
     void createEvent(Event event);
     void deleteEvent(String eventId);
