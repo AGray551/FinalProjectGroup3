1d4cc06b85c9f491bcf18bd1c36d7bf231e905de
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-11-25 22:05:39 -0500

    Update EventTrackerApplication.java

--- EventTracker/src/main/java/com/eventtracker/EventTrackerApplication.java
+++ EventTracker/src/main/java/com/eventtracker/EventTrackerApplication.java
@@ -1,1 +1,18 @@
 
+package com.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for Event Tracker application.
+ * This bootstraps the Spring Boot application.
+ */
+@SpringBootApplication
+public class EventTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTrackerApplication.class, args);
+    }
+}
+
