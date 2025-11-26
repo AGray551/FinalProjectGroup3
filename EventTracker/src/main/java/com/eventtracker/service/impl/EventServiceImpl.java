f84aa5c5836bf388d130dc6ad56d89859f1da0fc
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-11-25 22:16:13 -0500

    Create EventServiceImpl.java

---
+++ EventTracker/src/main/java/com/eventtracker/service/impl/EventServiceImpl.java
@@ -0,0 +1,41 @@
+
+package com.eventtracker.service.impl;
+
+import com.eventtracker.model.Event;
+import com.eventtracker.repository.EventRepository;
+import com.eventtracker.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private EventRepository eventRepo;
+
+    @Override
+    public List<Event> getAllEvents() {
+        return eventRepo.findAll();
+    }
+
+    @Override
+    public Event getEventById(String id) {
+        return eventRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void createEvent(Event event) {
+        eventRepo.save(event);
+    }
+
+    @Override
+    public void rsvpToEvent(String eventId, String userId) {
+        Event event = getEventById(eventId);
+        if (event != null) {
+            event.addAttendee(userId);
+            eventRepo.save(event);
+        }
+    }
+}
