267460164c8c04490c72f03525454e96e195d376
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-11-25 21:09:35 -0500

    Update IUserService.java

--- EventTracker/service/IUserService.java
+++ EventTracker/service/IUserService.java
@@ -1,9 +1,24 @@
+
 package service;
- 
+
 import model.User;
- 
+
+/**
+ * Service interface for managing users.
+ * Provides methods to retrieve and create users.
+ */
 public interface IUserService {
+
+    /**
+     * Retrieves a user by their ID.
+     * @param userId The ID of the user.
+     * @return User object if found, otherwise null.
+     */
     User getUserById(String userId);
+
+    /**
+     * Creates a new user.
+     * @param user The User object to create.
+     */
     void createUser(User user);
 }
- 
