a408916a912bc19dc0db8543f33133c25318d713
sidsiddu <85114685+sidsiddu@users.noreply.github.com>
2025-11-25 21:13:07 -0500

    Update UserServiceTest.java

--- EventTracker/test/UserServiceTest.java
+++ EventTracker/test/UserServiceTest.java
@@ -1,26 +1,29 @@
+
 package test;
- 
+
 import mock.MockUserService;
 import model.User;
 import org.junit.jupiter.api.Test;
- 
 import static org.junit.jupiter.api.Assertions.*;
- 
+
+/**
+ * Tests for MockUserService using BDD style (Given-When-Then).
+ */
 public class UserServiceTest {
- 
+
     @Test
-    public void givenUserwhenCreateUserthenUserCanBeRetrieved() {
+    public void givenUser_whenCreateUser_thenUserCanBeRetrieved() {
         // Given
         MockUserService userService = new MockUserService();
         User user = new User("1", "Riddhi", "riddhi@example.com");
- 
+
         // When
         userService.createUser(user);
         User retrieved = userService.getUserById("1");
- 
+
         // Then
-        assertNotNull(retrieved);
-        assertEquals("Riddhi", retrieved.getName());
+        assertNotNull(retrieved, "Retrieved user should not be null");
+        assertEquals("Riddhi", retrieved.getName(), "User name should match");
+        assertEquals("riddhi@example.com", retrieved.getEmail(), "User email should match");
     }
 }
- 
